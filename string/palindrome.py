0755c6f1855f69da34ff95b17e9473343e06e7f2
napulmi-hyh <napulmi@gmail.com>
2026-04-18 19:59:06 +0900

    Create palindrome.py

---
+++ string/palindrome.py
@@ -0,0 +1,15 @@
+def isPalindrome(s:str)->bool:
+    strs=[]
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+
+    # 팬린드롬 여부 판별
+    while len(strs)>1:
+        if strs.pop(0) != strs.pop():
+            return False
+    return True
+
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
